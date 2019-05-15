33edf08912f9080c01d26ad25ce8bc78322834eb
Josue Montaño <jmontano@biosoftinc.com>
2019-05-15 16:03:57 -0700

    Finishing insertData function. Ready for debug

--- src/controllers/TestFormsController.js
+++ src/controllers/TestFormsController.js
@@ -9,51 +9,82 @@ async function insertData(req, res) {
 	const operador = await dbInteract.isExists(`SELECT * FROM Operador WHERE id_operador=${body.operator}`);
 	// console.log(operador.result)
 	if(operador == false) {
-		res.send({
-			message: 'The operator doesn\'t exists'
-		});
-		return;
+		await require('./OperatorController').addOperator(body.operator, res);
 	}
 
 	const prueba = await dbInteract.isExists(`SELECT * FROM Pruebas WHERE nombre='${body.test}'`);
 	// console.log(prueba.result)
 	if (prueba == false) {
 		res.send({
-			message: 'The test doesn\'t exists'
+			message: 'El test no existe'
 		});
 		return;
-	}
-	
-	if(body.attributes != undefined){
-		for await (const atribute of body.attributes){
-			const atributo = await dbInteract.isExists(`SELECT * FROM Atributos WHERE nombre='${atribute.name}'`);
-
-			// console.log(atributo.result)
-		}
 	}
 
 	for await (const sample of body.samples){
 		const muestra = await dbInteract.isExists(`SELECT * FROM Muestras WHERE nombre='${sample}'`);
+
 		// console.log(muestra.result)
 		if (muestra == false && prueba.result.id_prueba > 1) {
 			res.send({
-				message: 'La muestra ' + sample + ' no existe'
-			});
-			return;
+				message: 'La muestra' + sample + ' no existe'
+			})
+			return
 		} else {
-			if (prueba.result.id_prueba > 1){
-				// console.log('insert log')
-				await require('./LogController').addLog(muestra.result.id_muestra, res);
+			if (prueba.result.id_prueba > 1) {
+				if(body.attributes != undefined) {
+					for await (const attribute of body.attributes){
+						const atributo = await dbInteract.isExists(`SELECT * FROM Atributos WHERE nombre='${attribute.name}'`);
+						
+						if (atributo == false) {
+							res.send({
+								message: 'El atributo ' + attribute.name + ' no existe'
+							});
+							return;
+						} else {
+							const valorMuestra = {
+								id_muestra: muestra.result.id_muestra,
+								id_atributo: atributo.result.id_atributo,
+								valor: attribute.value
+							}
+
+							await require('./SampleValueController').addSampleValue(valorMuestra, res);
+						}
+					}
+				}
+
+				for await (const state of body.states){
+					const estado = await dbInteract.isExists(`SELECT * FROM Estado WHERE nombre='${state}'`);
+
+					const log = {
+						id_operador: parseInt(body.operator, 10),
+						id_prueba: prueba.result.id_prueba,
+						id_muestra: muestra.result.id_muestra,
+						id_estado: estado.result.id_estado
+					}
+
+					await require('./LogController').addLog(log, res);
+				}
 			} else {
-				console.log('insert sample')
-				// await require('./SampleController').addSample(sample, res);
+				for await (const state of body.states){
+					const estado = await dbInteract.isExists(`SELECT * FROM Estado WHERE nombre='${state}'`);
+					const newSample = await require('./SampleController').addSample(sample, res)
+
+					const log = {
+						id_operador: parseInt(body.operator, 10),
+						id_prueba: prueba.result.id_prueba,
+						id_muestra: parseInt(newSample.id_muestra, 10),
+						id_estado: estado.result.id_estado
+					}
+
+					await require('./LogController').addLog(log, res);
+				}
 			}
 		}
 	}
 
-
 	res.send({
-		message: body
+		message: 'Inserción exitosa'
 	})
 }
 
